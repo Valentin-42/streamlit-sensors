b63b6783731e173d855b637a0e04f289daed8bce
Valentin <valentinvial42.vv@gmail.com>
2024-01-28 18:07:29 -0500

    GPS + Heading

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="streamlit-sensors",
-    version="0.0.9",
+    version="0.1.1",
     author="Valentin Vial",
     author_email="valentinvialent@gmail.com",
     description="Get geolocation and heading",
